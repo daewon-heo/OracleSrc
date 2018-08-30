ae8e8d304a534982a7c8c7387c54272a5d31dbfd
daewon-heo <eo7655@naver.com>
2018-08-30 20:39:03 +0900

    과제 커밋

--- 과제_워크샵3.sql
+++ 과제_워크샵3.sql
@@ -29,4 +29,126 @@ WHERE (STUDENT_ADDRESS LIKE '%경기도%'
     OR STUDENT_ADDRESS LIKE '%강원도%')
     AND SUBSTR(STUDENT_NO,1,1) = '9'
 ORDER BY 1;
+
+--4. 현재 법학과 교수 중 가장 나이가 많은 사람부터 이름을 확인할 수 있는 SQL 문장을
+--작성하시오. (법학과의 '학과코드'는 학과 테이블(TB_DEPARTMENT)을 조회해서 찾아
+--내도록 하자)
+select * from TB_DEPARTMENT; -- 법학과 005
+SELECT 
+    PROFESSOR_NAME,
+    PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE DEPARTMENT_NO = 005
+ORDER BY 2;
+
+--5. 2004 년 2 학기에 'C3118100' 과목을 수강한 학생들의 학점을 조회하려고 핚다. 학점이
+--높은 학생부터 표시하고, 학점이 같으면 학번이 낮은 학생부터 표시하는 구문을
+--작성해보시오.
+SELECT 
+    STUDENT_NO,
+    TO_CHAR(POINT, '9.99') POINT
+FROM TB_GRADE
+WHERE CLASS_NO = 'C3118100'
+AND TERM_NO = '200402'
+ORDER BY 2 DESC, 1;
+
+
+--6. 학생 번호, 학생 이름, 학과 이름을 이름 가나다 순서로 정렬하여 출력하는 SQL 문을
+--작성하시오.
+SELECT
+    STUDENT_NO,
+    STUDENT_NAME,
+    DEPARTMENT_NAME
+FROM TB_STUDENT
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+ORDER BY 2;
+
+--7. 춘 기술대학교의 과목 이름과 과목의 학과 이름을 학과 이름 가나다 순서로 출력하는
+--SQL 문장을 작성하시오.
+SELECT
+    CLASS_NAME,
+    DEPARTMENT_NAME
+FROM TB_CLASS
+JOIN TB_DEPARTMENT USING (DEPARTMENT_NO)
+ORDER BY 1;
+
+--8. 과목별 교수 이름을 찾으려고 한다. 과목 이름과 교수 이름을 출력하는 SQL 문을
+--작성하시오.
+
+SELECT 
+    CLASS_NAME,
+    PROFESSOR_NAME
+FROM TB_CLASS_PROFESSOR
+JOIN TB_CLASS USING(CLASS_NO)
+JOIN TB_PROFESSOR USING (PROFESSOR_NO);
+
+--9. 8 번의 결과 중 ‘인문사회’ 계열에 속한 과목의 교수 이름을 찾으려고 핚다. 이에
+--해당하는 과목 이름과 교수 이름을 출력하는 SQL 문을 작성하시오.
+SELECT 
+    CLASS_NAME,
+    PROFESSOR_NAME
+FROM TB_CLASS_PROFESSOR
+JOIN TB_CLASS USING(CLASS_NO)
+JOIN TB_PROFESSOR P USING (PROFESSOR_NO)
+JOIN TB_DEPARTMENT D ON P.DEPARTMENT_NO = D.DEPARTMENT_NO
+WHERE CATEGORY = '인문사회';
+
+--10. ‘음악학과’ 학생들의 평점을 구하려고 한다. 음악학과 학생들의 "학번", "학생 이름",
+--"전체 평점"을 평점이 높은 순서(평점이 동일하면 학번 순서)로 출력하는 SQL 문장을
+--작성하시오. (단, 평점은 소수점 1 자리까지만 반올림하여 표시핚다.)
+SELECT
+    TS.STUDENT_NO 학번,
+    TS.STUDENT_NAME "학생 이름",
+    ROUND(AVG(TG.POINT),1) "전체 평점"
+FROM TB_GRADE TG
+JOIN TB_STUDENT TS ON TG.STUDENT_NO = TS.STUDENT_NO
+JOIN TB_DEPARTMENT TD ON TD.DEPARTMENT_NO = TS.DEPARTMENT_NO
+WHERE TD.DEPARTMENT_NAME = '음악학과'
+GROUP BY TS.STUDENT_NO, TS.STUDENT_NAME
+ORDER BY 3 DESC, 1;
+
+--11. 학번이 A313047 인 학생이 학교에 나오고 있지 않다. 지도 교수에게 내용을 젂달하기
+--위한 학과 이름, 학생 이름과 지도 교수 이름이 필요하다. 이때 사용할 SQL 문을
+--작성하시오. 단, 출력헤더는 ‚학과이름‛, ‚학생이름‛, ‚지도교수이름‛으로
+--출력되도록 한다.
+SELECT  
+    TD.DEPARTMENT_NAME 학과이름,
+    TS.STUDENT_NAME 학생이름,
+    TP.PROFESSOR_NAME 지도교수이름
+FROM TB_STUDENT TS
+JOIN TB_PROFESSOR TP ON TS.COACH_PROFESSOR_NO = TP.PROFESSOR_NO
+JOIN TB_DEPARTMENT TD ON TD.DEPARTMENT_NO = TP.DEPARTMENT_NO
+WHERE STUDENT_NO = 'A313047';
+
+
+--12. 2007 년도에 '인간관계론' 과목을 수강한 학생을 찾아 학생이름과 수강학기를 이름
+--가나다 순서로 표시하는 SQL 문장을 작성하시오.
+SELECT 
+    TS.STUDENT_NAME,
+    TG.TERM_NO
+FROM TB_GRADE TG
+JOIN TB_STUDENT TS ON TS.STUDENT_NO = TG.STUDENT_NO
+JOIN TB_CLASS TC ON TC.CLASS_NO = TG.CLASS_NO
+WHERE SUBSTR(TERM_NO,1,4) = 2007
+AND CLASS_NAME = '인간관계론'
+ORDER BY 1;
+
+
+--13. 예체능 계열 과목 중 과목 담당교수를 한 명도 배정받지 못한 과목을 찾아 그 과목
+--이름과 학과 이름을 출력하는 SQL 문장을 작성하시오.
+
+SELECT 
+    CLASS_NAME,
+    DEPARTMENT_NAME
+FROM TB_CLASS
+LEFT JOIN TB_CLASS_PROFESSOR USING(CLASS_NO)
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+WHERE CATEGORY = '예체능'
+AND PROFESSOR_NO IS NULL
+ORDER BY 1;
+
+
+
+
+
     
